143c17dc2afc000c1f86716467f81442433ca8e5
Barhobbit18 <60877867+Barhobbit18@users.noreply.github.com>
2020-04-22 22:44:56 +0200

    Update Top20.py
    clean up

--- Top20.py
+++ Top20.py
@@ -12,7 +12,6 @@ i=0
 for t in tr[0]:
     i+=1
     col=t.text_content()
-
     frame.append((col,[]))
 
 for j in range(1,len(tr)):
@@ -25,14 +24,9 @@ for j in range(1,len(tr)):
     for t in T.iterchildren():
         data=t.text_content()
 
-
-        #print(data)
-
-        data = data.strip().split(' ')
         frame[i][1].append(data)
         i+=1
 
-        #print(frame)
 
 Dict={title:column for (title,column) in frame}
 df=pd.DataFrame(Dict)
@@ -40,5 +34,4 @@ df = df.replace(r'\n','', regex=True)
 df.columns=df.columns.str.replace('\n','')
 
 #print(df)
-#df[0]=data
 df.to_csv('Top20.csv')
